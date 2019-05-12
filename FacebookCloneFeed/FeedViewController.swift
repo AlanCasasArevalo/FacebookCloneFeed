991395f0510cb156405cf3efce3fa0897ceee9a5
Alan Casas <alancasasarevalo@gmail.com>
2019-05-12 09:30:21 +0200

    ++

---
+++ FacebookCloneFeed/FeedViewController.swift
@@ -0,0 +1,74 @@
+//
+//  FeedViewController.swift
+//  FacebookCloneFeed
+//
+//  Created by Alan Casas on 2019-05-12.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "Facebook Feed"
+        collectionView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
