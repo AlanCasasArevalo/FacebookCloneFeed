100b2b52ec12ddbdeee6bb435875a8830b7e54e6
Alan Casas <alancasasarevalo@gmail.com>
2019-05-12 17:43:54 +0200

    [FEATURE] [label] Label was added

--- FacebookCloneFeed/FeedCell.swift
+++ FacebookCloneFeed/FeedCell.swift
@@ -14,9 +14,46 @@ class FeedCell: UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "_DummyText"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     private func setupViews() {
         backgroundColor = UIColor.white
+        addSubview(nameLabel)
+        let horizontalConstraint = NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel])
+        let verticalConstraint = NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel])
+        addConstraints(horizontalConstraint)
+        addConstraints(verticalConstraint)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
