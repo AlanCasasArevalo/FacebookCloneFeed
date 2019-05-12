25f20ec6232ded52862669433eed574f3739a594
Alan Casas <alancasasarevalo@gmail.com>
2019-05-12 19:15:49 +0200

    Detail text was added

--- FacebookCloneFeed/FeedCell.swift
+++ FacebookCloneFeed/FeedCell.swift
@@ -17,7 +17,9 @@ class FeedCell: UICollectionViewCell {
 
     let nameLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 2
         let attributedText = NSMutableAttributedString(string: "_Beth", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "\nDecember 18 • San Francisco • ", attributes:  [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 12), NSAttributedString.Key.foregroundColor: UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)]))
         label.attributedText = attributedText
         return label
     }()
