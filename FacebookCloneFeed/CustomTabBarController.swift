103cde8442b36c450cea465b09d9049927475efe
Alan Casas <alancasasarevalo@gmail.com>
2019-05-15 19:50:16 +0200

    custom Tab bar controller was added

---
+++ FacebookCloneFeed/CustomTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTabBarController.swift
+//  FacebookCloneFeed
+//
+//  Created by Alan Casas on 15/05/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let feedController = FeedCollectionViewController()
+        let feedNVC = UINavigationController(rootViewController: feedController)
+        //UIApplicationShortcutIcon.IconType.love
+        feedNVC.title = "News feed"
+        feedNVC.tabBarItem.image = UIImage(named: "feed")
+        
+        let friendsController = UIViewController()
+        let friendsNVC = UINavigationController(rootViewController: friendsController)
+        friendsNVC.title = "Friend request"
+        friendsNVC.tabBarItem.image = UIImage(named: "friends")
+        viewControllers = [feedNVC, friendsNVC]
+    }
+    
+}
+
+
+
